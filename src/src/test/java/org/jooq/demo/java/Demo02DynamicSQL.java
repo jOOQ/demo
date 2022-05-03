69e39c292d6d799740f7947c04fcf49ba9c40ee0
Lukas Eder <lukas.eder@gmail.com>
2022-05-03 16:37:05 +0200

    [#1] Continue adding demo code: Add initial kotlin demo code

---
+++ src/src/test/java/org/jooq/demo/java/Demo02DynamicSQL.java
@@ -0,0 +1,69 @@
+package org.jooq.demo.java;
+
+import org.jetbrains.annotations.NotNull;
+import org.jooq.*;
+import org.jooq.demo.java.db.Tables;
+import org.jooq.impl.DSL;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.jooq.demo.java.db.Tables.ACTOR;
+import static org.jooq.impl.DSL.*;
+
+public class Demo02DynamicSQL extends AbstractDemo {
+
+    @Test
+    public void testDynamicSQL() {
+        title("Every jOOQ query is a dynamic SQL query. You just don't see it");
+        ctx.select(ACTOR.FIRST_NAME, ACTOR.LAST_NAME)
+            .from(ACTOR)
+            .where(ACTOR.ACTOR_ID.in(1L, 2L, 3L))
+            .orderBy(ACTOR.FIRST_NAME)
+            .limit(5)
+            .fetch();
+
+        title("The above and the below are equivalent");
+        List<SelectField<?>> select = List.of(ACTOR.FIRST_NAME, ACTOR.LAST_NAME);
+        Table<?> from = ACTOR;
+        Condition where = ACTOR.ACTOR_ID.in(1L, 2L, 3L);
+        List<OrderField<?>> orderBy = List.of(ACTOR.FIRST_NAME);
+        Field<Integer> limit = val(5);
+
+        ctx.select(select)
+           .from(from)
+           .where(where)
+           .orderBy(orderBy)
+           .limit(limit)
+           .fetch();
+
+        title("Any 'static' query part can be replaced by an expression, function call, etc.");
+        List<Integer> ids = List.of(1, 2, 3);
+
+        ctx.select(ACTOR.FIRST_NAME, ACTOR.LAST_NAME)
+           .from(ACTOR)
+           .where(ids.isEmpty()
+                  ? noCondition()
+                  : ACTOR.ACTOR_ID.in(ids.stream().map(Long::valueOf).map(DSL::val).toList()))
+           .orderBy(ACTOR.FIRST_NAME)
+           .limit(5)
+           .fetch();
+    }
+
+    @Test
+    public void generateQueryParts() {
+        println(reduceCondition(List.of()));
+        println(reduceCondition(List.of(1)));
+        println(reduceCondition(List.of(1, 2, 3)));
+    }
+
+    @NotNull
+    private Condition reduceCondition(List<Integer> ids) {
+        title("List: " + ids);
+        return ids
+            .stream()
+            .map(Long::valueOf)
+            .map(ACTOR.ACTOR_ID::eq)
+            .reduce(noCondition(), Condition::or);
+    }
+}
