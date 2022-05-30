02fa3f385548b7f1e4031ad4420f79f8c4502bfc
Lukas Eder <lukas.eder@gmail.com>
2022-05-30 10:31:37 +0200

    [jOOQ/demo#1] Changed artifactIds to be the same for pro and oss edition

--- jOOQ-demo-pro/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo12StoredProcedures.java
+++ jOOQ-demo-pro/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo12StoredProcedures.java
@@ -1,12 +1,18 @@
 package org.jooq.demo.java;
 
+import org.jooq.Variable;
 import org.jooq.demo.AbstractDemo;
 import org.jooq.demo.java.db.Routines;
+import org.junit.After;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 import static org.jooq.demo.java.db.Routines.inventoryInStock;
 import static org.jooq.demo.java.db.Tables.*;
-import static org.jooq.impl.DSL.lateral;
+import static org.jooq.impl.DSL.*;
+import static org.jooq.impl.SQLDataType.BIGINT;
+import static org.jooq.impl.SQLDataType.INTEGER;
 
 public class Demo12StoredProcedures extends AbstractDemo {
 
@@ -31,5 +37,31 @@ public class Demo12StoredProcedures extends AbstractDemo {
            .orderBy(FILM.FILM_ID, STORE.STORE_ID)
            .limit(10)
            .fetch();
+    }
+
+    @Test
+    public void proceduralLanguage() {
+        title("The procedural language API allows for creating procedures or anonymous blocks");
+
+        // This is a commercial only feature. Check out the commercial demo for details
+        /* [pro] */
+        Variable<Long> i = var("i", BIGINT);
+        ctx.begin(
+                for_(i).in(201L, 210L).loop(
+                    insertInto(ACTOR)
+                        .columns(ACTOR.ACTOR_ID, ACTOR.FIRST_NAME, ACTOR.LAST_NAME)
+                        .values(i, concat(inline("F"), i), concat(inline("L"), i))
+                )
+           )
+            .execute();
+
+        ctx.selectFrom(ACTOR).where(ACTOR.ACTOR_ID.gt(200L)).fetch();
+        /* [/pro] */
+    }
+
+    @After
+    public void teardown() throws SQLException {
+        cleanup(ACTOR, ACTOR.ACTOR_ID);
+        super.teardown();
     }
 }
