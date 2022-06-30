7ee0e0f2fedd613307698f0a33248eea4b728bd2
Lukas Eder <lukas.eder@gmail.com>
2022-06-30 16:55:25 +0200

    Re-generated code

---
+++ jOOQ-demo-oss/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo15ComputedColumns.java
@@ -0,0 +1,37 @@
+package org.jooq.demo.java;
+
+import org.jooq.*;
+import org.jooq.demo.AbstractDemo;
+import org.jooq.demo.java.db.tables.Actor;
+import org.jooq.demo.java.db.tables.FilmActor;
+import org.jooq.demo.java.db.tables.records.ActorRecord;
+import org.jooq.impl.DSL;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.jooq.Records.intoMap;
+import static org.jooq.Records.mapping;
+import static org.jooq.demo.java.db.Tables.*;
+import static org.jooq.impl.DSL.*;
+import static org.jooq.impl.SQLDataType.LOCALDATE;
+
+public class Demo15ComputedColumns extends AbstractDemo {
+
+    // Computed columns are a commercial only feature
+
+//
+//    @Test
+//    public void fetchVirtualClientSideComputedColumns() {
+//        ctx.select(CUSTOMER.FULL_NAME, CUSTOMER.FULL_ADDRESS)
+//            .from(CUSTOMER)
+//            .fetch();
+//    }
+//
+//
+
+}
