4c5150978d04ab626a35d904322df157d3a3a0fa
Lukas Eder <lukas.eder@gmail.com>
2024-01-09 14:36:17 +0100

    [#11] Demo code should link to relevant sections of the manual - Demo02DynamicSQL

--- jOOQ-demo-oss/jOOQ-demo-kotlin/src/test/kotlin/org/jooq/demo/kotlin/Demo02DynamicSQL.kt
+++ jOOQ-demo-oss/jOOQ-demo-kotlin/src/test/kotlin/org/jooq/demo/kotlin/Demo02DynamicSQL.kt
@@ -45,6 +45,10 @@ class Demo02DynamicSQL : AbstractDemo() {
             .orderBy(ACTOR.FIRST_NAME)
             .limit(5)
             .fetch()
+
+        // More information:
+        // - https://www.jooq.org/doc/latest/manual/sql-building/dynamic-sql/
+        // - https://www.jooq.org/doc/latest/manual/sql-building/dynamic-sql/no-condition/
     }
 
     @Test
@@ -55,10 +59,19 @@ class Demo02DynamicSQL : AbstractDemo() {
     }
 
     private fun reduceCondition(ids: List<Int>): Condition {
+        // For some operators like AND and OR, there exists an identity (TRUE for AND, and FALSE for OR), but often
+        // a pseudo-identity that completely omits an unnecessary clause is more useful. For this, we're using the
+        // DSL.noCondition(), which behaves like the identity of AND or OR, respectively, and doesn't generate a WHERE
+        // clause by itself.
+
         title("List: $ids")
         return ids
             .map { it.toLong() }
             .map { ACTOR.ACTOR_ID.eq(it) }
             .fold(noCondition()) { c1: Condition, c2: Condition -> c1.or(c2) }
+
+        // More information:
+        // - https://www.jooq.org/doc/latest/manual/sql-building/dynamic-sql/no-condition/
+        // - https://www.jooq.org/doc/latest/manual/sql-building/conditional-expressions/true-false-no-condition/
     }
 }
--- jOOQ-demo-pro/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo02DynamicSQL.java
+++ jOOQ-demo-pro/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo02DynamicSQL.java
@@ -49,6 +49,10 @@ public class Demo02DynamicSQL extends AbstractDemo {
            .orderBy(ACTOR.FIRST_NAME)
            .limit(5)
            .fetch();
+
+        // More information:
+        // - https://www.jooq.org/doc/latest/manual/sql-building/dynamic-sql/
+        // - https://www.jooq.org/doc/latest/manual/sql-building/dynamic-sql/no-condition/
     }
 
     @Test
@@ -60,11 +64,20 @@ public class Demo02DynamicSQL extends AbstractDemo {
 
     @NotNull
     private Condition reduceCondition(List<Integer> ids) {
+        // For some operators like AND and OR, there exists an identity (TRUE for AND, and FALSE for OR), but often
+        // a pseudo-identity that completely omits an unnecessary clause is more useful. For this, we're using the
+        // DSL.noCondition(), which behaves like the identity of AND or OR, respectively, and doesn't generate a WHERE
+        // clause by itself.
+
         title("List: " + ids);
         return ids
             .stream()
             .map(Long::valueOf)
             .map(ACTOR.ACTOR_ID::eq)
             .reduce(noCondition(), Condition::or);
+
+        // More information:
+        // - https://www.jooq.org/doc/latest/manual/sql-building/dynamic-sql/no-condition/
+        // - https://www.jooq.org/doc/latest/manual/sql-building/conditional-expressions/true-false-no-condition/
     }
 }
