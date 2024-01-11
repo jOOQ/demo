f000b528bb9b78578f081989afdf6b184883937f
Lukas Eder <lukas.eder@gmail.com>
2024-01-11 13:28:40 +0100

    [#11] Demo code should link to relevant sections of the manual - Demo07Parser

--- jOOQ-demo-pro/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo07Parser.java
+++ jOOQ-demo-pro/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo07Parser.java
@@ -16,18 +16,29 @@ public class Demo07Parser extends AbstractDemo {
 
     @Test
     public void parser() {
+        // jOOQ has a built-in parser for a grammar that is compatible with most of jOOQ's supported dialects.
+        // This helps to parse arbitrary SQL into a jOOQ expression tree, e.g. to apply further transformations on it,
+        // or make it vendor-agnostic, etc. The following statement uses Oracle style SQL, but it will work just fine
+        // on the configured PostgreSQL connection.
+
         title("jOOQ can also parse SQL strings to jOOQ expression trees");
         Select<?> select = ctx.parser().parseSelect("select instr('abcd', 'bc'), instr('a a a a a', 'a', 3) from dual");
         println(select);
         select.fetch();
+
+        // More information here:
+        // https://www.jooq.org/doc/latest/manual/sql-building/sql-parser/
+        // https://www.jooq.org/doc/latest/manual/sql-building/sql-parser/sql-parser-grammar/
     }
 
     @Test
     public void parsingConnection() throws SQLException {
+        // You don't even need to use jOOQ API to access the parser. Use it behind the scenes, as a JDBC or R2DBC proxy!
         title("You can use jOOQ behind the scenes to parse your legacy SQL statements in order to benefit a migration");
 
         // Replace your existing JDBC Connection by this one transparently, and suddenly, most of your
-        // SQL queries written for dialect A now work on any jOOQ dialect
+        // SQL queries written for dialect A now work on any jOOQ dialect. This doesn't mean you have to use JDBC
+        // directly. You can use any JDBC (or R2DBC) backed API, including e.g. JPA, Spring's JdbcClient, etc.
         try (Connection c = ctx.parsingConnection();
              PreparedStatement s = c.prepareStatement("select level from dual connect by level <= ?")) {
             s.setInt(1, 10);
@@ -37,6 +48,9 @@ public class Demo07Parser extends AbstractDemo {
                     println(rs.getInt(1));
             }
         }
+
+        // More information here:
+        // https://www.jooq.org/doc/latest/manual/sql-execution/parsing-connection/
     }
 
     @Test
@@ -77,6 +91,10 @@ public class Demo07Parser extends AbstractDemo {
 
     @Test
     public void parseListener() {
+        // The ParseListener is an SPI that allows for parsing vendor-specific SQL that currently isn't supported by
+        // jOOQ's parser. The below examples allow for parsing APPROX_SUM() or APPROX_AVG() and translate those to
+        // ordinary SUM() and AVG() aggregate functions.
+
         // Feature available in the commercial editions only
         /* [pro] */
         title("The jOOQ parser might not support all of your vendor specific syntax. In some cases, you can simply extend it");
@@ -101,5 +119,8 @@ public class Demo07Parser extends AbstractDemo {
 
         ctx.parser().parseSelect("select approx_avg(length) from film").fetch();
         /* [/pro] */
+
+        // More information here:
+        // https://www.jooq.org/doc/latest/manual/sql-building/sql-parser/sql-parser-listener/
     }
 }
