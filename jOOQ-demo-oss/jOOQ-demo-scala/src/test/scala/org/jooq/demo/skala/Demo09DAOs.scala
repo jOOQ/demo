351f535e554fb4f5ece18d3ef55667aa925d203f
Lukas Eder <lukas.eder@gmail.com>
2024-01-11 13:43:16 +0100

    [#11] Demo code should link to relevant sections of the manual - Demo09DAOs

--- jOOQ-demo-oss/jOOQ-demo-scala/src/test/scala/org/jooq/demo/skala/Demo09DAOs.scala
+++ jOOQ-demo-oss/jOOQ-demo-scala/src/test/scala/org/jooq/demo/skala/Demo09DAOs.scala
@@ -12,16 +12,30 @@ class Demo09DAOs extends AbstractDemo {
 
   @Test
   def pojos(): Unit = {
+    // jOOQ can generate a Plain Old Java Object (POJO) for each table, in multiple styles, including JavaBeans
+    // style POJOs, records, and more. These POJOs can be used as Data Transfer Objects (DTO) between modules
+    // without exposing any jOOQ API, such as jOOQ's TableRecord, for example. Unlike jOOQ's Records, however,
+    // POJOs do not implement any dirty tracking, optimistic locking, etc., so their usefulness is limited.
+
     title("jOOQ's code generator produces a POJO for every table")
     val actors = ctx
       .selectFrom(ACTOR)
       .where(ACTOR.ACTOR_ID.lt(4L))
       .fetchInto(classOf[Actor])
     actors.forEach(println(_))
+
+    // More information here:
+    // https://www.jooq.org/doc/latest/manual/sql-execution/fetching/pojos/
+    // https://www.jooq.org/doc/latest/manual/sql-execution/fetching/pojos-with-recordmapper-provider/
   }
 
   @Test
   def daos(): Unit = {
+    // A DAO is a generated class that maps between a generated Table and the generated POJO, and offers a few
+    // convenience methods for common CRUD operations, based directly on those POJOs. This can be useful for
+    // faster implementation of trivial CRUD. They're not meant as a replacement for actual querying with jOOQ!
+    // See also https://blog.jooq.org/to-dao-or-not-to-dao/
+
     title("Daos further simplify CRUD when working with jOOQ")
     val dao = new ActorDao(ctx.configuration)
 
@@ -29,6 +43,10 @@ class Demo09DAOs extends AbstractDemo {
       new Actor(201L, "John", "Doe", null),
       new Actor(202L, "Jane", "Smith", null))
     dao.fetchByActorId(201L, 202L).forEach(println(_))
+
+    // More information here:
+    // https://www.jooq.org/doc/latest/manual/sql-execution/daos/
+    // https://blog.jooq.org/to-dao-or-not-to-dao/
   }
 
   @After
--- jOOQ-demo-pro/jOOQ-demo-kotlin/src/test/kotlin/org/jooq/demo/kotlin/Demo09DAOs.kt
+++ jOOQ-demo-pro/jOOQ-demo-kotlin/src/test/kotlin/org/jooq/demo/kotlin/Demo09DAOs.kt
@@ -11,6 +11,11 @@ class Demo09DAOs : AbstractDemo() {
 
     @Test
     fun pojos() {
+        // jOOQ can generate a Plain Old Java Object (POJO) for each table, in multiple styles, including JavaBeans
+        // style POJOs, records, and more. These POJOs can be used as Data Transfer Objects (DTO) between modules
+        // without exposing any jOOQ API, such as jOOQ's TableRecord, for example. Unlike jOOQ's Records, however,
+        // POJOs do not implement any dirty tracking, optimistic locking, etc., so their usefulness is limited.
+
         title("jOOQ's code generator produces a data class for every table")
         val actors = ctx
             .selectFrom(ACTOR)
@@ -18,10 +23,19 @@ class Demo09DAOs : AbstractDemo() {
             .fetchInto(Actor::class.java)
 
         actors.forEach { println(it) }
+
+        // More information here:
+        // https://www.jooq.org/doc/latest/manual/sql-execution/fetching/pojos/
+        // https://www.jooq.org/doc/latest/manual/sql-execution/fetching/pojos-with-recordmapper-provider/
     }
 
     @Test
     fun daos() {
+        // A DAO is a generated class that maps between a generated Table and the generated POJO, and offers a few
+        // convenience methods for common CRUD operations, based directly on those POJOs. This can be useful for
+        // faster implementation of trivial CRUD. They're not meant as a replacement for actual querying with jOOQ!
+        // See also https://blog.jooq.org/to-dao-or-not-to-dao/
+
         title("Daos further simplify CRUD when working with jOOQ")
         val dao = ActorDao(ctx.configuration())
         dao.insert(
@@ -30,6 +44,10 @@ class Demo09DAOs : AbstractDemo() {
         )
 
         dao.fetchByActorId(201L, 202L).forEach { println(it) }
+
+        // More information here:
+        // https://www.jooq.org/doc/latest/manual/sql-execution/daos/
+        // https://blog.jooq.org/to-dao-or-not-to-dao/
     }
 
     @After
