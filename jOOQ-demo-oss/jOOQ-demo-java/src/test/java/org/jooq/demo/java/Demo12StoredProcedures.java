78e134d0fb21bc495d6f556d0db4e1f77305b091
Lukas Eder <lukas.eder@gmail.com>
2024-01-11 14:25:19 +0100

    [#11] Demo code should link to relevant sections of the manual - Demo12StoredProcedures

--- jOOQ-demo-oss/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo12StoredProcedures.java
+++ jOOQ-demo-oss/jOOQ-demo-java/src/test/java/org/jooq/demo/java/Demo12StoredProcedures.java
@@ -16,6 +16,10 @@ public class Demo12StoredProcedures extends AbstractDemo {
 
     @Test
     public void procedures() {
+        // Calling stored procedures and functions ("routines") is very easy with jOOQ and its code generator. Every
+        // routine has a corresponding generated stub in Java, which you can call with a Configuration, and jOOQ will
+        // take care of binding all the IN/OUT parameters.
+
         title("Standalone procedure calls require a configuration argument");
         println("Inventory 1 in stock: " + Routines.inventoryInStock(configuration, 1L));
 
@@ -35,6 +39,19 @@ public class Demo12StoredProcedures extends AbstractDemo {
            .orderBy(FILM.FILM_ID, STORE.STORE_ID)
            .limit(10)
            .fetch();
+
+        // More information here:
+        // - https://www.jooq.org/doc/latest/manual/code-generation/codegen-procedures/
+        // - https://www.jooq.org/doc/latest/manual/sql-execution/stored-procedures/
+        //
+        // And also various use-cases described in these blog posts:
+        // - https://blog.jooq.org/the-best-way-to-call-stored-procedures-from-java-with-jooq/
+        // - https://blog.jooq.org/calling-procedures-with-default-parameters-using-jdbc-or-jooq/
+        // - https://blog.jooq.org/postgresqls-table-valued-functions/
+        // - https://blog.jooq.org/access-pl-sql-procedures-from-java-with-jooq-a-jpublisher-alternative/
+        // - https://blog.jooq.org/use-jooq-to-read-write-oracle-plsql-record-types/
+        // - https://blog.jooq.org/using-oracle-aq-in-java-wont-get-any-easier-than-this/
+        // - https://blog.jooq.org/how-to-pass-a-table-valued-parameter-to-a-t-sql-function-with-jooq/
     }
 
     @Test
@@ -55,6 +72,9 @@ public class Demo12StoredProcedures extends AbstractDemo {
 //
 //        ctx.selectFrom(ACTOR).where(ACTOR.ACTOR_ID.gt(200L)).fetch();
 //
+
+        // More information here:
+        // - https://www.jooq.org/doc/latest/manual/sql-building/procedural-statements/
     }
 
     @After
--- jOOQ-demo-oss/jOOQ-demo-scala/src/test/scala/org/jooq/demo/skala/Demo12StoredProcedures.scala
+++ jOOQ-demo-oss/jOOQ-demo-scala/src/test/scala/org/jooq/demo/skala/Demo12StoredProcedures.scala
@@ -15,6 +15,10 @@ class Demo12StoredProcedures extends AbstractDemo {
 
   @Test
   def procedures(): Unit = {
+    // Calling stored procedures and functions ("routines") is very easy with jOOQ and its code generator. Every
+    // routine has a corresponding generated stub in Java, which you can call with a Configuration, and jOOQ will
+    // take care of binding all the IN/OUT parameters.
+
     title("Standalone procedure calls require a configuration argument")
     println("Inventory 1 in stock: " + Routines.inventoryInStock(configuration, 1L))
 
@@ -37,11 +41,25 @@ class Demo12StoredProcedures extends AbstractDemo {
       .orderBy(FILM.FILM_ID, STORE.STORE_ID)
       .limit(10)
       .fetch
+
+    // More information here:
+    // - https://www.jooq.org/doc/latest/manual/code-generation/codegen-procedures/
+    // - https://www.jooq.org/doc/latest/manual/sql-execution/stored-procedures/
+    //
+    // And also various use-cases described in these blog posts:
+    // - https://blog.jooq.org/the-best-way-to-call-stored-procedures-from-java-with-jooq/
+    // - https://blog.jooq.org/calling-procedures-with-default-parameters-using-jdbc-or-jooq/
+    // - https://blog.jooq.org/postgresqls-table-valued-functions/
+    // - https://blog.jooq.org/access-pl-sql-procedures-from-java-with-jooq-a-jpublisher-alternative/
+    // - https://blog.jooq.org/use-jooq-to-read-write-oracle-plsql-record-types/
+    // - https://blog.jooq.org/using-oracle-aq-in-java-wont-get-any-easier-than-this/
+    // - https://blog.jooq.org/how-to-pass-a-table-valued-parameter-to-a-t-sql-function-with-jooq/
   }
 
   @Test
   def proceduralLanguage(): Unit = {
     title("The procedural language API allows for creating procedures or anonymous blocks")
+
     // This is a commercial only feature. Check out the commercial demo for details
 
 //    val i = `var`("i", BIGINT)
@@ -53,6 +71,9 @@ class Demo12StoredProcedures extends AbstractDemo {
 //      .execute
 //    ctx.selectFrom(ACTOR).where(ACTOR.ACTOR_ID.gt(200L)).fetch
 //
+
+    // More information here:
+    // - https://www.jooq.org/doc/latest/manual/sql-building/procedural-statements/
   }
 
   @After
